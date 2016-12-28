460a13f0056dccb0b23d67567c7f93aa5d8b7495
Robert M Flight <r.flight79@gmail.com>
2016-12-28 13:05:25 -0500

    setting and getting the cran and bioconductor mirrors

--- R/reup.R
+++ R/reup.R
@@ -1,3 +1,33 @@
+#' set cran mirror
+#'
+#' Sets the CRAN mirror to use. Default is the RStudio one
+#'
+#' @param url the url of the CRAN mirror
+#'
+#' @export
+#' @return NULL
+set_cran_mirror <- function(url = "https://cloud.r-project.org/"){
+  assign("cran_mirror", url, envir = reup_options)
+}
+
+get_cran_mirror <- function(){
+  get("cran_mirror", envir = reup_options)
+}
+
+#' set bioconductor mirror
+#'
+#' Sets the Bioconductor mirror to use. Default is https://bioconductor.org
+#'
+#' @export
+#' @return NULL
+set_bioc_mirror <- function(url = "https://bioconductor.org"){
+  assign("bioc_mirror", url, envir = reup_options)
+}
+
+get_bioc_mirror <- function(){
+  get("bioc_mirror", envir = reup_options)
+}
+
 #' reup
 #'
 #' actually tries to do the upgrade of packages using `install.packages`.
