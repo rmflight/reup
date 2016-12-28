5be5510bd408a856787877f7702b0ba5a34ec288
Robert M Flight <r.flight79@gmail.com>
2016-12-27 22:22:48 -0500

    just the very first function, that does nothing currently

---
+++ R/reup.R
@@ -0,0 +1,10 @@
+#' reup
+#'
+#' actually tries to do the upgrade of packages using `install.packages`.
+#'
+#' @param ... various options, normally set using the setter functions
+#'
+#' @export
+reup <- function(...){
+
+}
