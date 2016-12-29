3939409ac38bdd8badce701192d0aa05e97ede6d
Robert M Flight <r.flight79@gmail.com>
2016-12-28 23:05:22 -0500

    starting to work on getting the list of old packages

---
+++ R/get_package_lists.R
@@ -0,0 +1,11 @@
+#' old packages
+#'
+#' get the list of packages installed in the old library
+#'
+#' @param old_library where to look
+#'
+#' @export
+#' @return matrix
+get_old_packages <- function(old_library = get_old_library()) {
+
+}
