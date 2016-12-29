094a6beead54d55e6aa9e762a66fe7534f4dc1f9
Robert M Flight <r.flight79@gmail.com>
2016-12-28 22:13:29 -0500

    all of the necessary infrastructure for testing setting of local packages

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,12 @@
 reup_options <- new.env(hash = TRUE)
+
+reset_reup_options <- function(){
+  curr_names <- names(reup_options)
+
+  if (length(curr_names) > 0) {
+    for (iname in curr_names) {
+      assign(iname, NULL, envir = reup_options)
+    }
+  }
+
+}
---
+++ tests/testthat/test-local_packages.R
@@ -0,0 +1,26 @@
+context("local packages")
+
+test_that("local packages works correctly", {
+  old_libpaths <- .libPaths()
+  old_libs <- Sys.getenv("R_LIBS")
+
+  tmp_libpath <- file.path(tempdir(), "reup_test", "old_libs")
+
+  if (!dir.exists(tmp_libpath)) dir.create(tmp_libpath, recursive = TRUE)
+  .libPaths(c(tmp_libpath, .libPaths()))
+  Sys.setenv(R_LIBS = tmp_libpath)
+
+  on.exit({
+    .libPaths(old_libpaths)
+    Sys.setenv(R_LIBS = old_libs)
+  }, add = TRUE)
+
+  # reset everything
+  reset_reup_options()
+  # should have an error because new library is not set
+  expect_error(set_local_packages())
+
+  set_new_library()
+  # still error because devtools isnt installed
+  expect_error(set_local_packages())
+})
